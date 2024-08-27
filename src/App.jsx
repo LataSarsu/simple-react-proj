f09e1a4bd1838f0263ff0aeef2c2f6007043f157
Lata Ramachandra Sarsu <lata.sarsu@gyansys.com>
2024-08-27 19:24:02 +0530

    second commit

--- src/App.jsx
+++ src/App.jsx
@@ -4,15 +4,12 @@ import viteLogo from "/vite.svg";
 import "./App.css";
 import Box from "./components/Box";
 
-function App({winVal}) {
+function App() {
   const [count, setCount] = useState(0);
 
   return (
     <>
-      <div>
-        <h1>Roll less than 4 {winVal}</h1>
-        <Box />
-      </div>
+      <Box onSet />
     </>
   );
 }
--- src/components/Box.jsx
+++ src/components/Box.jsx
@@ -1,38 +1,46 @@
 import React from "react";
 import { useState } from "react";
+import PropTypes from "prop-types";
 
 function randomIntFromInterval(min, max) {
   // min and max included
   return Math.floor(Math.random() * (max - min + 1) + min);
 }
 
-const Box = () => {
-  const [value, setValue] = useState(randomIntFromInterval(1, 6));
-  let rndInt = randomIntFromInterval(1, 6);
-  let rndInt1 = randomIntFromInterval(1, 6);
-  var winVal;
-
+const Box = ({ onSet }) => {
+  // const [value, setValue] = useState(randomIntFromInterval(1, 6));
+  // let rndInt = randomIntFromInterval(1, 6);
+  // let rndInt1 = randomIntFromInterval(1, 6);
+  // var winVal;
+  const [val1, setVal1] = useState();
+  const [val2, setVal2] = useState();
+  onSet = (val1, val2) => {
+    if (val1 < 4 && val2 < 4) {
+      const winVal = "You win!";
+      return winVal;
+    }
+  };
   return (
     <>
-      <div className="bigBox">
-        <p className="smallBox">{value}</p>
-        <p className="smallBox">{rndInt1}</p>
+      <div>
+        <h1>Roll less than 4 {winVal}</h1>
+        <div className="bigBox">
+          <p className="smallBox">{val1}</p>
+          <p className="smallBox">{val2}</p>
+        </div>
+        <button
+          className="bbtn"
+          onClick={(setVal1 = () => {
+            val1 = randomIntFromInterval(1, 6);
+          })(
+            (setVal2 = () => {
+              val2 = randomIntFromInterval(1, 6);
+            })
+          )}
+        >
+          Re-roll
+        </button>
       </div>
-      <button
-        className="bbtn"
-        onClick={
-          (setValue = () => {
-            rndInt = randomIntFromInterval(1, 6);
-            rndInt1 = randomIntFromInterval(1, 6);
-            if ({ rndInt } < 4 && { rndInt1 } < 4) {
-              winVal = "You Win!";
-              return winVal;
-            }
-          })
-        }
-      >
-        Re-roll
-      </button>
     </>
   );
 };
